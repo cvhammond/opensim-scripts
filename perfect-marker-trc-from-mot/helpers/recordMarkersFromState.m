eeca3f6de3fea76fd05726b93f17769c2c413cdc
Claire V. Hammond <cvhammond@mailbox.org>
2023-03-11 22:43:32 -0600

    Update recordMarkersFromState.m

--- perfect-marker-trc-from-mot/helpers/recordMarkersFromState.m
+++ perfect-marker-trc-from-mot/helpers/recordMarkersFromState.m
@@ -7,7 +7,7 @@
 % Return a RowVectorOfVec3 of marker positions for a given state
 function row = recordMarkersFromState(model, state)
 import org.opensim.modeling.*
-row = RowVectorOfVec3(model.getMarkerSet().getSize());
+row = RowVectorVec3(model.getMarkerSet().getSize());
 for i=0:model.getMarkerSet().getSize()-1
     m = model.getMarkerSet().get(i);
     row.set(i,m.getLocationInGround(state));
